220e6103dd37527c0364011ee5650b704758323a
GiandomenicoIameo <g.iameo@studenti.unina.it>
2025-02-27 12:44:03 +0100

    update

--- client/client-tcp.py
+++ client/client-tcp.py
@@ -1,43 +1,73 @@
 from socket import *
 
+def send( socket, message ):
+
+    socket.send( message.encode() )
+    print( 'Messaggio inviato!' )
+
+    return socket.recv( 1024 )
+
+def authentication( head ):
+
+    username = input( 'Username: ' )
+    password = input( 'Password: ')
+
+    message = head + username + ' ' + password
+    return message
+
+def release( head, action ):
+
+    if action == 1:
+        return authentication( head )
+    return head + ''
+
+def search( head ):
+
+    filmname = input( 'Nome film: ' )
+    message = head + filmname
+    return message
+
+def request():
+
+    method = input( 'Inserisci il tipo di richiesta: ' )
+    method = int( method )
+
+    if method == 3:
+        action = 0
+    else:
+        action = input( 'Inserisci il tipo di azione: ' )
+        action = int( action )
+
+    return method, action
+
 server_name = 'localhost'
 server_port = 8080
 
 client_socket = socket( AF_INET, SOCK_STREAM )
 client_socket.connect( ( server_name, server_port ) )
 
-method = input( 'Inserisci il tipo di richiesta: ' )
-action = input( 'Inserisci il tipo di azione: ' )
+while True:
 
-method = int( method )
-action = int( action )
+    method, action = request()
+    head = str( method ) + str( action )
 
-if method == 0:
-    if action == 0 or action == 1:
-        username = input( 'Username: ' )
-        password = input( 'Password: ')
-
-        body = username + ' ' + password
-        message = str( method ) + str( action ) + body
-elif method == 2:
-    if action == 1:
-        username = input( 'Username: ' )
-        password = input( 'Password: ')
-
-        body = username + ' ' + password
-        message = str( method ) + str( action ) + body
+    if method == 0:
+        message = authentication( head )
+    elif method == 2:
+        message = release( head, action )
+        if action == 0 or action == 1:
+            break
+    elif method == 3:
+        message = search( head )
     else:
-        message = str( method ) + str( action )
+        message = head + body
+        break
 
-elif method == 3:
-    film = input( 'Nome film: ' )
-    message = str( method ) + str( action ) + film
+    receive_message = send( client_socket, message )
+    print( 'Messaggio ricevuto dal server:', receive_message.decode() )
 
-client_socket.send( message.encode() )
-
-print( 'Messaggio inviato!' )
-
-receive_message = client_socket.recv( 1024 )
+print( 'messaggio da inviare:', message )
+receive_message = send( client_socket, message )
 print( 'Messaggio ricevuto dal server:', receive_message.decode() )
 
 client_socket.close()
